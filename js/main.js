96a0635c7f33d0085f6355dce550874bce6e2886
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-11 22:25:50 -0300

    add ticker list function

--- js/main.js
+++ js/main.js
@@ -21,8 +21,17 @@ const handleAddTicker = async (event) => {
         const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=6OU8R697MHZQST38`)
         const data = await response.json()
         console.log(data)
-        if (data["Global Quote"]["05. price"]) {
-            alert('Deu certo.')
+        const price = data["Global Quote"]["05. price"]
+        if (price) {
+            const newTicker = 
+            `<div class="ticker">
+                <h2>${ticker}</h2>
+                <p>${price}</p>
+            </div>`
+
+            const tickerList = document.querySelector("#ticker-list")
+            tickerList.innerHTML += newTicker
+            closeModal('#add-stock')
         } else {
             alert(`Ticker ${ticker} não encontrado!`)
         }
