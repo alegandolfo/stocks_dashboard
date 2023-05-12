baa0200ac4a3199fa41fcf6378288b85d2d775f5
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-11 22:10:06 -0300

    create fetch api stocks

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const openModal = (idModal) => {
+    const divModal = document.querySelector(idModal)
+    divModal.style.display = "flex"
+}
+
+const closeModal = (idModal) => {
+    const divModal = document.querySelector(idModal)
+    divModal.style.display = "none"
+}
+
+const handleModalClose = (event) => {
+    if(event.target.className === "modal") {
+        event.target.style.display = "none"
+    }
+}
+
+const handleAddTicker = async (event) => {
+    event.preventDefault()
+    const ticker = event.target.ticker.value
+    try {
+        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=6OU8R697MHZQST38`)
+        const data = await response.json()
+        console.log(data)
+        if (data["Global Quote"]["05. price"]) {
+            alert('Deu certo.')
+        } else {
+            alert(`Ticker ${ticker} não encontrado!`)
+        }
+    } catch(error) {
+        alert(error)
+    }
+}
+
+const modal = document.querySelector(".modal")
+modal.addEventListener("click", handleModalClose)
