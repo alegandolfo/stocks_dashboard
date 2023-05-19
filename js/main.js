58e657142664b90fbf39e21dd71e9139c8d7c2e4
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-18 21:38:52 -0300

    add remove ticker

--- js/main.js
+++ js/main.js
@@ -20,17 +20,32 @@ const handleAddTicker = async (event) => {
     try {
         const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=6OU8R697MHZQST38`)
         const data = await response.json()
-        console.log(data)
         const price = data["Global Quote"]["05. price"]
-        if (price) {
+        const previousClosePrice = data["Global Quote"]["08. previous close"]
+        if (price && previousClosePrice) {
+            const priceFormatted = parseFloat(price).toFixed(2)
+            const previousClosePriceFormatted = parseFloat(previousClosePrice).toFixed(2)
+            let priceChange = ''
+            let Symbol = ''
+            if (priceFormatted !== previousClosePriceFormatted) {
+                if (priceFormatted > previousClosePriceFormatted) {
+                    priceChange = 'increase'
+                    Symbol = '▲'
+                } else {
+                    priceChange = 'decrease'
+                    Symbol = '▼'
+                }
+            }
             const newTicker = 
             `<div class="ticker">
+                <button class="btn-close" onclick="removeTicker(event)">x</button>
                 <h2>${ticker}</h2>
-                <p>${price}</p>
+                <p class="${priceChange}">${Symbol} U$ ${priceFormatted}</p>
             </div>`
 
-            const tickerList = document.querySelector("#ticker-list")
-            tickerList.innerHTML += newTicker
+            const tickerList = document.querySelector("#tickers-list")
+            tickerList.innerHTML = newTicker + tickerList.innerHTML
+            addTickerCloseEvent()
             closeModal('#add-stock')
         } else {
             alert(`Ticker ${ticker} não encontrado!`)
@@ -40,5 +55,33 @@ const handleAddTicker = async (event) => {
     }
 }
 
+const handleTickerMouseEnter = (event) => {
+    const ticker = event.target
+    const btnClose = ticker.querySelector(".btn-close")
+    btnClose.style.display = "block"
+}
+
+const handleTickerMouseLeave = (event) => {
+    const ticker = event.target
+    const btnClose = ticker.querySelector(".btn-close")
+    btnClose.style.display = "none"
+}
+
+const addTickerCloseEvent = () => {
+    const tickers = document.querySelectorAll(".ticker")
+    tickers.forEach((ticker, index) => {
+        ticker.addEventListener("mouseenter", handleTickerMouseEnter)
+        ticker.addEventListener("mouseleave", handleTickerMouseLeave)
+    })
+}
+
+const removeTicker = (event) => {
+    const btnClose = event.target
+    const ticker = btnClose.closest('.ticker')
+    ticker.remove()
+}
+
 const modal = document.querySelector(".modal")
 modal.addEventListener("click", handleModalClose)
+
+addTickerCloseEvent()
